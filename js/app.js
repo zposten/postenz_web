d10e3e7cbac95edb0b3df3b96987324133dcf6ae
Zach Posten <zposten@gmail.com>
2015-10-18 09:24:04 -0500

    Attempt to dynamically display title information

--- js/app.js
+++ js/app.js
@@ -15,20 +15,26 @@ WebsiteApp.config(['$routeProvider', function($routeProvider) {
            templateUrl: 'views/blog.html'
        })
        .otherwise({
-           redirectTo: '/blog'
+           redirectTo: '/'
        });
 }]);
 
 WebsiteApp.controller('HomeController', function($scope) {
     $scope.message = 'This is the home screen';
+    $scope.title = 'Zach Posten'
+    $scope.subtitle = 'Sometimes I just want to give it all up and become a handsome billionaire.'
 });
 
 WebsiteApp.controller('ContactController', function($scope) {
     $scope.message = 'This is the controller screen';
+    $scope.title = 'Contact Information'
+    $scope.subtitle = 'How you\'ll reach me when you inevitably want to';
 });
 
 WebsiteApp.controller('BlogController', function($scope) {
     $scope.message = 'This is the blog screen';
+    $scope.title = 'My Blog';
+    $scope.subtitle = 'My most inner thoughts about public matters';
 });
 
 
