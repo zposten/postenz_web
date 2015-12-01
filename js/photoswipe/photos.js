29265c69d209c67e519dd17df4a9bf2382d91de5
Zach Posten <zposten@gmail.com>
2015-11-30 19:55:17 -0600

    Add more photos

--- js/photoswipe/photos.js
+++ js/photoswipe/photos.js
@@ -44,9 +44,217 @@ photoswipe.photos = [
         caption: 'Sunrise'
     },
     {
-        href: 'https://farm1.staticflickr.com/604/22829660459_b9d278a60e_k.jpg',
+        href: 'https://farm6.staticflickr.com/604/22829660459_b9d278a60e_k.jpg',
         size: '2048x1365',
-        thumbnail: 'https://farm1.staticflickr.com/604/22829660459_f91c6ba2ec_n.jpg',
+        thumbnail: 'https://farm6.staticflickr.com/604/22829660459_f91c6ba2ec_n.jpg',
         caption: 'Jumping fences in the Badlands'
+    },
+
+
+
+    // ROUND TWO
+    {
+        href: 'https://farm1.staticflickr.com/627/23381479276_cab7b61bc7_k.jpg',
+        size: '2048x1288',
+        thumbnail: 'https://farm1.staticflickr.com/627/23381479276_d867913567_n.jpg',
+        caption: 'Zeus Stick'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/760/22780479413_ce3d54ca8e_k.jpg',
+        size: '2048x1613',
+        thumbnail: 'https://farm1.staticflickr.com/760/22780479413_093f525ec1_n.jpg',
+        caption: 'Little Monster'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/656/23111876790_cbfc8fdeb9_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/656/23111876790_08dd831ffe_n.jpg',
+        caption: 'White Lights'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/599/22779283344_29890fab3a_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/599/22779283344_cbfaa8bc74_n.jpg',
+        caption: 'Christmas Tree'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5629/23407658115_cfa1899b10_k.jpg',
+        size: '2048x879',
+        thumbnail: 'https://farm1.staticflickr.com/5629/23407658115_851dece750_n.jpg',
+        caption: 'Christmas Light'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/660/23407665635_3ab888138f_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/660/23407665635_5467419181_n.jpg',
+        caption: 'Sausages'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5718/23381508236_652f07c7bf_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/5718/23381508236_51da47a403_n.jpg',
+        caption: 'Lovely Tree'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/590/23325125471_641573bf72_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/590/23325125471_83bc47a199_n.jpg',
+        caption: 'Tree Branches'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/687/23039709779_d270aaed05_k.jpg',
+        size: '2048x1449',
+        thumbnail: 'https://farm1.staticflickr.com/687/23039709779_ce8df18e24_n.jpg',
+        caption: 'Flagpole'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5667/22779315044_bd520d4f3e_k.jpg',
+        size: '2048x1319',
+        thumbnail: 'https://farm1.staticflickr.com/5667/22779315044_493c12c3e8_n.jpg',
+        caption: 'Rock Bottom'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/631/22780522083_149f61dafd_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/631/22780522083_a707aecd1a_n.jpg',
+        caption: 'River Walk'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/724/23299220292_ce44e68386_k.jpg',
+        size: '2048x1272',
+        thumbnail: 'https://farm1.staticflickr.com/724/23299220292_d15b4826ac_n.jpg',
+        caption: 'Green Bridge'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/577/23111927680_b4df9d1a81_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/577/23111927680_3d92ca27e2_n.jpg',
+        caption: 'Marcus Fountain'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/660/23039734949_390c4f563d_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/660/23039734949_af60230e4e_n.jpg',
+        caption: 'City Hall'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5661/22779340084_2d3c80fb18_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/5661/22779340084_edcb3c4106_n.jpg',
+        caption: 'Circles'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5772/22779340844_9ff662a544_k.jpg',
+        size: '2048x1166',
+        thumbnail: 'https://farm1.staticflickr.com/5772/22779340844_0b1cc88a15_n.jpg',
+        caption: 'Lovely Eye'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/601/23039739019_65e91dd364_k.jpg',
+        size: '2048x1536',
+        thumbnail: 'https://farm1.staticflickr.com/601/23039739019_62f97be5e1_n.jpg',
+        caption: 'Dome'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/749/23325159451_23dde49dfd_k.jpg',
+        size: '2048x626',
+        thumbnail: 'https://farm1.staticflickr.com/749/23325159451_99408cc637_n.jpg',
+        caption: 'Horticulture Domes'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5676/23039741019_6961e4b701_k.jpg',
+        size: '2048x1152',
+        thumbnail: 'https://farm1.staticflickr.com/5676/23039741019_6be4d4e983_n.jpg',
+        caption: 'Halestorm'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/589/23407719575_cd22381894_k.jpg',
+        size: '1536x2048',
+        thumbnail: 'https://farm1.staticflickr.com/589/23407719575_634d30b58f_n.jpg',
+        caption: 'Lzzy'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/682/23381554166_21a37b03e9_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/682/23381554166_8d52d3c110_n.jpg',
+        caption: 'Kickball'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5759/22779353604_d4a3df4608_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/5759/22779353604_b179bf8c80_n.jpg',
+        caption: '21st Birthday'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5814/23325175331_45b5f34889_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/5814/23325175331_179376041f_n.jpg',
+        caption: 'Lovely Smile'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/680/23325181051_086faab56c_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/680/23325181051_8f445bd68b_n.jpg',
+        caption: 'Cub Picture'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/754/23111972930_19d4630326_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/754/23111972930_a7ba5f3ef2_n.jpg',
+        caption: 'Sunshine'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/720/22779380094_b2806be222_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/720/22779380094_fd951af43a_n.jpg',
+        caption: 'Shining Lights'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/670/22780587503_7f41467edd_k.jpg',
+        size: '2048x1266',
+        thumbnail: 'https://farm1.staticflickr.com/670/22780587503_aabbe4075b_n.jpg',
+        caption: 'Outside Wrigly'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/583/23381597936_3bbae6618d_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/583/23381597936_451077f7c8_n.jpg',
+        caption: 'Lucroy'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/630/23299295182_368fb4c1a3_k.jpg',
+        size: '2048x1260',
+        thumbnail: 'https://farm1.staticflickr.com/630/23299295182_e0f81b73ab_n.jpg',
+        caption: 'Braun'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/635/23039803089_462d397203_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/635/23039803089_5ed6de9010_n.jpg',
+        caption: 'Camera Guy'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/709/22780611703_17ac7e37c0_k.jpg',
+        size: '2048x1216',
+        thumbnail: 'https://farm1.staticflickr.com/709/22780611703_cac1dee1f2_n.jpg',
+        caption: 'Blue Dock'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5756/22780612953_55b06ca4d5_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/5756/22780612953_78da6eb9ec_n.jpg',
+        caption: 'Streamers'
+    },
+    {
+        href: 'https://farm1.staticflickr.com/5760/23039809719_4e7690032c_k.jpg',
+        size: '2048x1365',
+        thumbnail: 'https://farm1.staticflickr.com/5760/23039809719_32046ce5a1_n.jpg',
+        caption: 'Sparklers'
+    },
+    {
+        href: 'https://farm6.staticflickr.com/5664/22569126454_55fd8c781b_k.jpg',
+        size: '1365x2048',
+        thumbnail: 'https://farm6.staticflickr.com/5664/22569126454_bdcf19817e_n.jpg',
+        caption: 'Hills'
     }
 ];
